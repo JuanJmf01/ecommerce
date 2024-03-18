9dd04fbe02bbcca9510c540c268b405b4044482b
juanjmf01 <juaneltrozo13@gmail.com>
2024-03-18 12:04:58 -0500

    modified vite.config to work with new port and all reed interfaces

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,6 +2,14 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    port: 3000, // Puerto personalizado
+    host: '0.0.0.0', // Escuchar en todas las interfaces de red
+  },
+});
